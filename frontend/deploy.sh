301d3ee67e206ab2dbc984d7c7ef12ff7b831341
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2018-01-24 14:16:20 -0300

    Add front end confs
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,6 +1,9 @@
-git pull
-npm install
-bower update appcivist-patterns
-grunt build
+#!/bin/bash
+source ~/.bashrc
+cd /home/appcivist/production/appcivist-pb-client
+npm install -f
+bower -f update appcivist-patterns
+grunt build -f
 rm -rf /var/www/html/appcivist-pb/*
 cp -rf dist/* /var/www/html/appcivist-pb
+sudo apachectl -D FOREGROUND
