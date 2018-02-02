c2d939cccbfaa1445fc12f2570e7674f9d27cfcb
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2018-02-02 08:47:43 -0300

    remove usnb files before deploy
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- usnb/deploy_local/deploy.sh
+++ usnb/deploy_local/deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+rm -rf /home/appcivist/production/usnb/api-gateway/*
+rm -rf /home/appcivist/production/usnb/entity-manager/*
+rm -rf /home/appcivist/production/usnb/email-bindingcomponent/*
+rm -rf /home/appcivist/production/usnb/facebook-bindingcomponent/*
+rm -rf /home/appcivist/production/usnb/subscription-manager/*
+
 pm2 deploy entity-manager.ecosystem.config.js $1 setup
 pm2 deploy entity-manager.ecosystem.config.js $1
 pm2 deploy email-bindingcomponent.ecosystem.config.js $1 setup
