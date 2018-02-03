a9458f98d6910f15757c79846b5d47b75d92895c
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2018-02-03 16:19:29 -0300

    apache conf
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- appcivist-frontend/deploy.sh
+++ appcivist-frontend/deploy.sh
@@ -7,5 +7,6 @@ bower -f update appcivist-patterns
 grunt build -f
 rm -rf /var/www/html/appcivist-pb/*
 cp -rf dist/* /var/www/html/appcivist-pb
+echo "ServerName testappcivist.org" | sudo tee /etc/apache2/conf-available/fqdn.conf
 sudo service apache2 stop
-sudo apachectl -D FOREGROUND
+sudo service apache2 start && tail -f /dev/null
