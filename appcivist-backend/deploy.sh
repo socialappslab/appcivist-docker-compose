0d806d3ef3b95bb3a843238a83af8f085dce1ca1
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2018-02-16 22:04:00 -0300

    change directories
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- appcivist-backend/deploy.sh
+++ appcivist-backend/deploy.sh
@@ -3,4 +3,5 @@ cd /home/appcivist/production/appcivist-platform/conf/
 cp local.sample.conf local.conf
 cp play-authenticate/mine.sample.conf play-authenticate/mine.conf
 cp play-authenticate/smtp.sample.conf play-authenticate/smtp.conf
-/home/appcivist/production/appcivist-platform/./activator stage && /etc/init.d/appcivist-backend start
+cd /home/appcivist/production/appcivist-platform/
+./activator stage && /etc/init.d/appcivist-backend start
