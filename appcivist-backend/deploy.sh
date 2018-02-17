84eb2dcd7736319136d9c308319bc8a02481b8eb
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2018-02-16 21:35:51 -0300

    add appcivist env variables
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- appcivist-backend/deploy.sh
+++ appcivist-backend/deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+cd /home/appcivist/production/appcivist-platform/conf/
+cp local.sample.conf local.conf
+cp play-authenticate/mine.sample.conf play-authenticate/mine.conf
+cp play-authenticate/smtp.sample.conf play-authenticate/smtp.conf
 /home/appcivist/production/appcivist-platform/./activator stage && /etc/init.d/appcivist-backend start
