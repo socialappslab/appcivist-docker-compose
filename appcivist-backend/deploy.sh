a6cc4d8a77f36a5b7d93d98c17aa8c6090916f3b
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2018-02-17 16:19:37 -0300

    correct conf files names
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- appcivist-backend/deploy.sh
+++ appcivist-backend/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/appcivist/production/appcivist-platform/conf/
 cp local.sample.conf local.conf
-cp play-authenticate/mine.sample.conf play-authenticate/mine.conf
-cp play-authenticate/smtp.sample.conf play-authenticate/smtp.conf
+cp play-authenticate/mine.sample.conf play-authenticate/mine.local.conf
+cp play-authenticate/smtp.sample.conf play-authenticate/smtp.local.conf
 cd /home/appcivist/production/appcivist-platform/
 ./activator stage && /etc/init.d/appcivist-backend start
