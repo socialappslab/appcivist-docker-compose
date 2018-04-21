1407f76a137b76cd614bf550a149f8170c7cf047
AppCivist <appcivist@js-156-149.jetstream-cloud.org>
2018-04-21 17:34:18 -0400

    Voting api run

--- appcivist-voting-api/deploy.sh
+++ appcivist-voting-api/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/appcivist/production/appcivist-voting-api
-gem install rails -v 4.2.5
+gem install rails -v 4.2.7.1
 gem install bundler
+gem install pg
 bundle install
-sh /etc/init.d/appcivist-voting-api.sh start
+bundle update
+sh /etc/init.d/appcivist-voting-api.sh start && tail -f /dev/null
