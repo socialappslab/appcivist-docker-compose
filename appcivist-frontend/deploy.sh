db56bd81bbcb7f048dae918d023958ed01ee9e2d
Cristhian Parra <cdparra@gmail.com>
2018-10-25 23:45:21 -0300

    Always replace variables for sentry

--- appcivist-frontend/deploy.sh
+++ appcivist-frontend/deploy.sh
@@ -9,9 +9,10 @@ then
 else
     echo "Using sample env.sample.js"
     cp env.sample.js env.js
-    sed -i -e "s/SENTRY_SECRET/$SENTRY_SECRET/g" env.js
-    sed -i -e "s/SENTRY_PROJECT_ID/$SENTRY_PROJECT_ID/g" env.js
 fi
+
+sed -i -e "s/SENTRY_SECRET/$SENTRY_SECRET/g" env.js
+sed -i -e "s/SENTRY_PROJECT_ID/$SENTRY_PROJECT_ID/g" env.js
 
 cd /home/appcivist/production/appcivist-pb-client
 
