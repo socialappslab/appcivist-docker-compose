b55f2886dcc86c1ec60e05f0d7ef5e859f37a972
Cristhian Parra <cdparra@gmail.com>
2018-10-25 22:57:08 -0300

    Minor fix to deploy.sh

--- appcivist-frontend/deploy.sh
+++ appcivist-frontend/deploy.sh
@@ -10,8 +10,10 @@ else
     echo "Using sample env.sample.js"
     cp env.sample.js env.js
     sed -i -e "s/SENTRY_SECRET/$SENTRY_SECRET/g" env.js
-	sed -i -e "s/PROJECT_ID/$SENTRY_PROJECT_ID/g" env.js
+	sed -i -e "s/SENTRY_PROJECT_ID/$SENTRY_PROJECT_ID/g" env.js
 fi
+
+https://8bac15af9c954bc2b0633aaf0da007f2@sentry.io/SENTRY_PROJECT_ID
 
 cd /home/appcivist/production/appcivist-pb-client
 
