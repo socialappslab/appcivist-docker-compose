dc7f644ce2e5027fa4e2d7b4f36c51899a8ee345
Cristhian Parra <cdparra@gmail.com>
2018-10-25 22:39:30 -0300

    Added creation of env.js from sample + env variables for sentry reporting

--- appcivist-frontend/deploy.sh
+++ appcivist-frontend/deploy.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 source ~/.bashrc
+
+#!/bin/bash
+cd /home/appcivist/production/appcivist-pb-client/app
+if [ -e env.js ]
+then
+    echo "Using existing env.js"
+else
+    echo "Using sample env.sample.js"
+    cp env.sample.js env.js
+    sed -i -e "s/SENTRY_SECRET/$SENTRY_SECRET/g" env.js
+	sed -i -e "s/PROJECT_ID/$SENTRY_PROJECT_ID/g" env.js
+fi
+
 cd /home/appcivist/production/appcivist-pb-client
+
 npm install grunt --save-dev
 npm install -f 
 bower install
